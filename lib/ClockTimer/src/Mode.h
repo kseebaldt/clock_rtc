93365800c249c9b32af3d897bc5b86570f972e6e
Kurtis Seebaldt <kseebaldt@gmail.com>
2019-05-13 21:16:42 -0500

    add clock and timer mode

--- lib/ClockTimer/src/Mode.h
+++ lib/ClockTimer/src/Mode.h
@@ -1,9 +1,15 @@
+#ifndef _MODE_H_
+#define _MODE_H_
+
 #include <stdint.h>
 
 class Mode {
+    public:
     virtual uint16_t displayValue() = 0;
     virtual void button1() = 0;
     virtual void button2() = 0;
     virtual void button3() = 0;
     virtual void switch1(bool state) = 0;
 };
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,73 @@
 #include <Arduino.h>
 
 #include "BcdDriver.h"
+#include "Mode.h"
+
 #include "Clock.h"
+#include "Timer.h"
+
+#include "Button.h"
 
 BcdDriver driver(A3, A0, A1, A2, 4, 8, 9, 10, 12);
 Clock clock;
+Timer timer;
+
+Button button1 = Button(2);
+Button button2 = Button(3);
+Button button3 = Button(4);
+Button toggleSwitch = Button(5);
+Button modeSwitch = Button(6);
+
+Mode *currentMode;
+
+void button1Callback(Button& button) {
+  currentMode->button1();
+}
+
+void button2Callback(Button& button) {
+  currentMode->button2();
+}
+
+void button3Callback(Button& button) {
+  currentMode->button3();
+}
+
+void toggleSwitchCallback(Button& button) {
+  currentMode->switch1(button.state() == LOW);
+}
+
+void modeSwitchCallback(Button& button) {
+  if (button.state() == LOW) {
+      currentMode = (Mode *)&clock;
+  } else {
+      currentMode = (Mode *)&timer;
+  }
+}
 
 void setup() {
-  clock.start();
+  clock.init();
+  timer.init();
+
+  currentMode = (Mode *)&clock;
+
+  button1.init();
+  button2.init();
+  button3.init();
+  toggleSwitch.init();
+  modeSwitch.init();
 
   driver.initPins();
 }
 
 void loop() {
-  driver.setValue(clock.time());
+  timer.tick();
+
+  button1.tick();
+  button2.tick();
+  button3.tick();
+  toggleSwitch.tick();
+  modeSwitch.tick();
+
+  driver.setValue(currentMode->displayValue());
   driver.display();
 }
